ade82fe43e12b5aadc97bdf972f47c931194f4c7
IntoSpaceAgain <davidellwood@yahoo.co.uk>
2020-06-04 16:45:57 +0100

    Added planning comments

--- cointoss.py
+++ cointoss.py
@@ -1,2 +1,7 @@
 ''' Write some code that simulates flipping a single coin however many times the user decides. The code should record the outcomes and count the number of heads and tails. '''
 
+# User input = number of time to flip the coin
+
+# record the outcome of each in a list
+
+# count the number of heads and tails.
